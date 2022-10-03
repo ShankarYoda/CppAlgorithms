8a75f4189e62fc01168cff63d136a9327befe3e2
ShankarYoda <114861706+ShankarYoda@users.noreply.github.com>
2022-10-03 18:35:19 +0530

    kadanes.cpp

--- kadanes.cpp
+++ kadanes.cpp
@@ -17,13 +17,11 @@ int maxSubArraySum(int a[], int size)
     return max_so_far;
 }
 
-
 int main()
 {
     int a[] = {-2, -3, 4, -1, -2, 1, 5, -3};
     int n = sizeof(a) / sizeof(a[0]);
 
-   
     int max_sum = maxSubArraySum(a, n);
     cout << "Maximum contiguous sum is " << max_sum;
     return 0;
